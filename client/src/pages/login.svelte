c105edb27bc5b2444a227cfb405885dfe1abd034
lucasmodin <lumo0002@stud.kea.dk>
2025-11-21 13:24:18 +0100

    made barebone login svelte page

---
+++ client/src/pages/login.svelte
@@ -0,0 +1,28 @@
+<script>
+import { fetchPost } from '../util/fetchUtil';
+
+let username = "";
+let password = "";
+
+async function login() {
+    const result = await fetchPost("/auth/login", {
+        username,
+        password
+    });
+
+    if (result.error) {
+        return;
+    }
+}
+</script>
+
+<h1>Mechanicus Login</h1>
+
+<form on:submit={login}>
+
+    <input placeholder="Username" bind:value={username}>
+    <input type="password" placeholder="Password" bind:value={password}>
+
+    <button type="submit">Login</button>
+
+</form>
