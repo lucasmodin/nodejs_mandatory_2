c13a85a3e9e1410c89a1c48be581726d19874e5d
lucasmodin <lumo0002@stud.kea.dk>
2025-11-24 20:24:45 +0100

    fix

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,12 +1,12 @@
 import { fetchPost } from "../util/fetchUtil";
 import { session } from "../stores/sessionStore";
 import { navigate } from "svelte-routing";
-import { sanitize } from "./xssSanitizer";
+
 
 export async function login(username, password) {
-    const safeUsername = sanitize(username);
+    
     const result = await fetchPost("/auth/login", {
-        username: safeUsername,
+        username: username,
         password: password 
     });
 
